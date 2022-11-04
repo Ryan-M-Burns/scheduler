27d78b71a2836a5d86082efb79b2c94c59d6efc3
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-04 15:39:53 -0700

    create DayList skeleton

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DayList = (props) => {
+
+  const {
+    days, // array
+    day, // string
+    setDay // function
+  } = props;
+  
+  return (
+    <ul>
+
+    </ul>
+  );
+};
+
+export default DayList;

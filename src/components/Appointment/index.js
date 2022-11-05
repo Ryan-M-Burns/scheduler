0fb88c773063f85d78f07962ff90b31bc97d0418
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-05 14:31:15 -0700

    create Appointment subdirectory add styling and skeleton index

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const index = props => {
+
+  return (
+    <article className="appointment">
+
+    </article>
+
+  );
+
+};
+
+export default index;

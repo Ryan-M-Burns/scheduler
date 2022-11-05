1344145f6d9c0e4596d53375909f628c07644a65
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-05 15:17:56 -0700

    implement Empty and add story

--- stories/index.js
+++ stories/index.js
@@ -155,4 +155,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", props => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);

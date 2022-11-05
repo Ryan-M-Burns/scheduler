58f37c93063b0e55c36a0c73278396667cfc4533
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-05 16:36:33 -0700

    implement skeleton for Form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+
+const Form = props => {
+
+  const {
+    onCancel, // function
+    onSave, // function
+  } = props;
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+          /*
+            This must be a controlled component
+            your code goes here
+          */
+          />
+        </form>
+        <InterviewerList
+        /* your code goes here */
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={onCancel} >Cancel</Button>
+          <Button confirm onClick={onSave} >Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+};
+
+export default Form;

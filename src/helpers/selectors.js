1693d00eb7a8cf8b2e00357c9f2cad051c4b3369
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-06 15:14:37 -0800

    working selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,19 +1,16 @@
 // Ask mentor
 
 const getAppointmentsForDay = (state, day) => {
-  const appointments = state.days.filter(stateDay => stateDay.name === day);
-  const appointmentsForDay = [];
+  const dayObj = state.days.find(stateDay => stateDay.name === day);
 
-  if (appointments[0]) {
-    appointments.map(id => {
-      for (let appointment in state.appointments) {
-        if (id === appointment.id) {
-          appointsmentsForDay.push(appointment);
-        }
-      }
-    });
+  if (!dayObj) {
+    return [];
   }
-  return appointmentsForDay;
+  const dayAppointments = dayObj.appointments.map(id => {
+    return state.appointments[id.toString()];
+  });
+
+  return dayAppointments;
 };
 
 export { getAppointmentsForDay };

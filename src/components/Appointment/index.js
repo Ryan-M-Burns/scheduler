2cc1af20d8faf2707bf86f0e5337c7011aa40e9c
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-07 22:25:05 -0800

    Add Empty, Show, and Create conditional rendering statements

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,26 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode';
 
 const Appointment = props => {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { interview, time } = props;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  const interviewElement = () => {
+    { mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />; }
+    { mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} />; }
+    { mode === CREATE && <CREATE interviewers={[]} />; }
+  };
 
   return (
     <article className="appointment">
-      <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      <Header time={time} />
+      {interviewElement()}
     </article>
   );
 

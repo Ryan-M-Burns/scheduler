2557997af6798e278d3f0c740de8eaa4e89ad332
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-08 16:13:15 -0800

    implement interviews selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,32 @@
-// Ask mentor
-
 const getAppointmentsForDay = (state, day) => {
   const dayObj = state.days.find(stateDay => stateDay.name === day);
 
   if (!dayObj) {
     return [];
   }
+
   const dayAppointments = dayObj.appointments.map(id => {
     return state.appointments[id.toString()];
   });
 
   return dayAppointments;
 };
+
+
+const getInterviewersForDay = (state, day) => {
+  const dayObj = state.days.find(stateDay => stateDay.name === day);
+
+  if (!dayObj) {
+    return [];
+  }
+
+  const dayInterviewers = dayObj.interviewers.map(id => {
+    return state.interviewers[id.toString()];
+  });
+
+  return dayInterviewers;
+};
+
 
 const getInterview = (state, interview) => {
   if (!interview) {
@@ -23,4 +38,4 @@ const getInterview = (state, interview) => {
 };
 
 
-export { getAppointmentsForDay, getInterview };
+export { getAppointmentsForDay, getInterview, getInterviewersForDay };

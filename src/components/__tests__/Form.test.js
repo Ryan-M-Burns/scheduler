1c7fc09203c463e0a478fcd65a7581cd7bff1593
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-10 21:34:01 -0800

    change test to increase coverage

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -66,22 +66,20 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("calls onSave function when the name and interviewer is defined", () => {
-    /* 1. Create the mock onSave function */
+  it("submits the name entered by the user", () => {
     const onSave = jest.fn();
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-    const { queryByText } = render(
+    const { getByText, getByPlaceholderText } = render(
       <Form
         interviewers={interviewers}
         onSave={onSave}
-        interviewer={interviewers[0].id}
-        student="Lydia Miller-Jones"
+        interviewer={1}
       />
     );
-    /* 3. Click the save button */
-    fireEvent.click(queryByText("Save"));
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/please select an interviewer/i)).toBeNull();
+
+    const input = getByPlaceholderText("Enter Student Name");
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
